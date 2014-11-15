5c2e6347767ae7ae1f551fcdefbed56d77c9ed5e
Darshan Prasad <dprasad@gmail.com>
2014-11-15 13:24:31 -0800

    Added more examples of adding / removing edge case nodes

--- src/datastructures/DataStructures.java
+++ src/datastructures/DataStructures.java
@@ -23,6 +23,24 @@ public class DataStructures {
         list.add(n2);
         System.out.println(list.toString() + "\n\n");
         
+        System.out.println("Add a third node:");
+        LinkedListNode n3 = new LinkedListNode(7);
+        list.add(n3);
+        System.out.println(list.toString() + "\n\n");
+        
+        System.out.println("Add a fourth node:");
+        LinkedListNode n4 = new LinkedListNode(10);
+        list.add(n4);
+        System.out.println(list.toString() + "\n\n");
+        
+        System.out.println("Remove a node:");
+        list.remove(n2, (Integer) n2.getValue());
+        System.out.println(list.toString() + "\n\n");
+        
+        System.out.println("Remove an end node:");
+        list.remove(n4, (Integer) n4.getValue());
+        System.out.println(list.toString() + "\n\n");
+        
         System.out.println("Remove a head node:");
         list.remove(n1, (Integer) n1.getValue());
         System.out.println(list.toString() + "\n\n");
